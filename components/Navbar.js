ea1e160f07c8282430a0e3bf057c0377fcc825c2
jason-francois <jasonc.francois@gmail.com>
2022-06-02 22:41:02 -0400

    Added files that weren't in last commit

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Where in the World?</li>
+      </ul>
+      <ul>
+        <li>Where in the World?</li>
+      </ul>
+    </nav>
+  );
+}
