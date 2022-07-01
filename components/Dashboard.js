78ce979df25879f7199af1ac7177e2c1edf82297
jason-francois <jasonc.francois@gmail.com>
2022-06-30 22:53:44 -0400

    Implemented filter dropdown; Began SCSS refactoring

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Country from "./Country";
 import styles from "../styles/pages/Home.module.scss";
 import Search from "./Search";
-import { uuid } from "uuidv4";
+import { v4 as uuidv4 } from "uuid";
 
 const Dashboard = (props) => {
   const [countries, setCountries] = useState([]);
@@ -16,6 +16,7 @@ const Dashboard = (props) => {
 
   useEffect(() => {
     fetchCountries();
+    console.log(countries);
   }, []);
 
   const onChange = (e) => {
@@ -26,12 +27,25 @@ const Dashboard = (props) => {
       )
     );
   };
+  const onClick = (e) => {
+    const value = e.target.textContent;
+    const elements = document.querySelector(`.${styles.selected}`);
+    if (elements) {
+      elements.classList.remove(`${styles.selected}`);
+    }
+    e.target.classList.add(`${styles.selected}`);
+    setCountries(
+      initialCountries.filter(
+        (x) => x.region.toLowerCase() == value.toLowerCase()
+      )
+    );
+  };
   return (
     <>
-      <Search onChange={onChange} />
+      <Search onChange={onChange} onClick={onClick} />
       <div className={[styles.container]}>
         {countries.map((country, index) => (
-          <Country key={uuid()} flagData={country} />
+          <Country key={uuidv4()} flagData={country} />
         ))}
       </div>
     </>
--- components/Search.js
+++ components/Search.js
@@ -1,39 +1,58 @@
 import styles from "../styles/components/Search.module.scss";
 
 const Search = (props) => {
+  const onToggleClick = () => {
+    const options = document.querySelector(`.${styles["filterOptions"]}`);
+    options.classList.toggle(`${styles["fade"]}`);
+  };
   return (
     <>
-      <div className={styles["search-container"]}>
-        <i
-          className={`fa fa-search ${styles["search-icon"]}`}
-          aria-hidden="true"
-        />
-        <input
-          className={styles.search}
-          type="text"
-          placeholder="Search for a country..."
-          onChange={props.onChange}
-        />
-      </div>
-      <div className={styles["filter-container"]}>
-        <div className={styles.filter}>
-          <p
-            style={{
-              fontWeight: 400,
-              letterSpacing: "0.05px",
-              paddingRight: "22px",
-            }}
-          >
-            Filter by Region
-          </p>
-          <i id={styles["filter-toggle"]} className="fa fa-angle-down"></i>
+      <div style={{ marginBottom: "3rem" }}>
+        <div className={styles["search-container"]}>
+          <i
+            className={`fa fa-search ${styles["search-icon"]}`}
+            aria-hidden="true"
+          />
+          <input
+            className={styles.search}
+            type="text"
+            placeholder="Search for a country..."
+            onChange={props.onChange}
+          />
         </div>
-        <div className={styles.filterOptions}>
-          <p className={styles.filterOption}>Africa</p>
-          <p className={styles.filterOption}>America</p>
-          <p className={styles.filterOption}>Asia</p>
-          <p className={styles.filterOption}>Europe</p>
-          <p className={styles.filterOption}>Oceania</p>
+        <div className={styles["filter-container"]}>
+          <div className={styles.filter}>
+            <p
+              style={{
+                fontWeight: 400,
+                letterSpacing: "0.05px",
+                paddingRight: "22px",
+              }}
+            >
+              Filter by Region
+            </p>
+            <i
+              className={`fa fa-angle-down ${styles["filter-toggle"]}`}
+              onClick={onToggleClick}
+            ></i>
+          </div>
+          <div className={styles.filterOptions}>
+            <p className={styles.filterOption} onClick={props.onClick}>
+              Africa
+            </p>
+            <p className={styles.filterOption} onClick={props.onClick}>
+              America
+            </p>
+            <p className={styles.filterOption} onClick={props.onClick}>
+              Asia
+            </p>
+            <p className={styles.filterOption} onClick={props.onClick}>
+              Europe
+            </p>
+            <p className={styles.filterOption} onClick={props.onClick}>
+              Oceania
+            </p>
+          </div>
         </div>
       </div>
     </>
